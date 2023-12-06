3cd3702f237bb8d8dd1f5b8bfa0837fd049c32c5
Grim73 <grimreepr73@gmail.com>
2023-12-06 15:10:54 -0600

    uploaded file structure and start of solution for 2023 day 1

---
+++ 2023/day1/solution.py
@@ -0,0 +1,14 @@
+f = open("input.txt", "r")
+lines = f.readlines()
+## one line function adding each occurence of `lines` to the list `calories`
+value = [entry.strip() for entry in lines]
+print(value)
+
+container = []
+numbers = [1,2,3,4,5,6,7,8,9,0]
+for num in value:
+    print(num)
+    container.append(num)
+    print(num)
+print(value)
+print(container)
